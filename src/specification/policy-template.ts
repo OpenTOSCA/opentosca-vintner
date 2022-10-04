9c4a057a8ca1d407cad317ef439d9e0bfac29faf
Miles Stötzner <miles@stoetzner.de>
2022-10-04 17:50:14 +0200

    conditional policies

---
+++ src/specification/policy-template.ts
@@ -0,0 +1,14 @@
+import {VariabilityExpression} from './variability'
+
+/**
+ * Policy Template
+ * {@link https://docs.oasis-open.org/tosca/TOSCA-Simple-Profile-YAML/v1.3/os/TOSCA-Simple-Profile-YAML-v1.3-os.html#DEFN_ELEMENT_POLICY_DEF}
+ */
+
+export type PolicyTemplate = {
+    type: string
+    conditions?: VariabilityExpression | VariabilityExpression[]
+}
+
+export type PolicyTemplateList = PolicyAssignmentMap[]
+export type PolicyAssignmentMap = {[name: string]: PolicyTemplate}
