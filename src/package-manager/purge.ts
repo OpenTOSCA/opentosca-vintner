7d90f50ad2b3b66b001474762c31036db4365cdd
Mike Lauer <mike@lauercloud.de>
2022-10-31 12:17:41 +0100

    refactor

--- src/package-manager/purge.ts
+++ src/package-manager/purge.ts
@@ -6,7 +6,7 @@ import list from '../controller/templates/list'
 import {Dependencies} from './types'
 
 function main() {
-    const dependencyList = gatherAllDependencies()
+    const dependencyList = utils.gatherAllDependencies()
     console.log(dependencyList)
 
     const directories = files.readDir(LIB_DIRECTORY)
@@ -16,40 +16,6 @@ function main() {
             files.removeDirectory(path.join(TMP_DIRECTORY, dir))
             console.log(`Deleted ${dir}`)
         }
-    }
-}
-
-function gatherAllDependencies(): Set<string> {
-    const dependencyList = new Set<string>()
-    // Add all deps from root dependency file
-    const dependencies = utils.readDependencyFile()
-    addDependenciesToList(dependencies, dependencyList)
-
-    // Add all deps from sub dependency files
-    const directories = files.readDir(LIB_DIRECTORY)
-    let listSize
-    do {
-        listSize = dependencyList.size
-        for (const dir of directories) {
-            // Only consider directories of required dependencies
-            if (dependencyList.has(dir)) {
-                const subDependencyFilePath = path.join(TMP_DIRECTORY, dir, DEPENDENCY_FILE)
-
-                // If dependency has a dependency file
-                if (files.exists(subDependencyFilePath)) {
-                    const subDependencies = utils.readCustomDependencyFile(subDependencyFilePath)
-                    addDependenciesToList(subDependencies, dependencyList)
-                }
-            }
-        }
-    } while (listSize != dependencyList.size) // Repeat until no more changes
-
-    return dependencyList
-}
-
-function addDependenciesToList(dependencies: Dependencies, list: Set<string>) {
-    for (const dep of dependencies) {
-        list.add(utils.getDirectoryNameForDependency(dep))
     }
 }
 
