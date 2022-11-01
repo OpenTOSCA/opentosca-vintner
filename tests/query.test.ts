4e52369211fa98f9689ab3ce697f6f41f37b9221
JustinKissling <xjustinx@gmx.net>
2022-11-01 12:39:49 +0100

    more query tests
    Former-commit-id: 538b49c663742b050227c0dde04e6e05e775530a

--- tests/query.test.ts
+++ tests/query.test.ts
@@ -1,17 +1,52 @@
 import {expect} from 'chai'
 import * as files from '../src/utils/files';
 import path from 'path';
-import {Resolver} from '../src/query/resolver'
-import {QueryTemplateArguments} from '../src/controller/query/resolve';
+import executeQuery from '../src/controller/query/resolve';
 
 it('all', () => {
     const result = getResult('FROM template/tests/query/service-template.yaml SELECT .')
     expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/all/expected-output.yaml')))
 })
 
+it('filter-equals', () => {
+    const result = getResult
+    ('FROM template/tests/query/service-template.yaml SELECT node_templates.*[type="VirtualMachine"].name')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/filter-equals/expected-output.yaml')))
+})
+
+it('filter-regex', () => {
+    const result = getResult
+    ('FROM template/tests/query/service-template.yaml SELECT node_templates.*[name=~"vm_"]')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/filter-regex/expected-output.yaml')))
+})
+
 it('group', () => {
     const result = getResult('FROM template/tests/query/service-template.yaml SELECT GROUP(database_group)')
     expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/group/expected-output.yaml')))
+})
+
+it('match-next', () => {
+    const result = getResult
+    ('FROM template/tests/query/service-template.yaml MATCH ([type="Tomcat"])-->(node) SELECT node')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/match-next/expected-output.yaml')))
+})
+
+it('match-previous', () => {
+    const result = getResult
+    ('FROM template/tests/query/service-template.yaml MATCH (node)<--([type="Tomcat"]) SELECT node')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/match-previous/expected-output.yaml')))
+})
+
+it('match-single', () => {
+    const result = getResult
+    ('FROM template/tests/query/service-template.yaml MATCH (node[type="WebApplication"]) SELECT node')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/match-single/expected-output.yaml')))
+})
+
+it('match-variable-length', () => {
+    const result = getResult
+    ('FROM template/tests/query/service-template.yaml MATCH ([type="WebApplication"])-{*}->(node[type=Compute]) SELECT node')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/match-variable-length/expected-output.yaml')))
 })
 
 it('node-template', () => {
@@ -24,8 +59,16 @@ it('policy', () => {
     expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/policy/expected-output.yaml')))
 })
 
+it('result-structure-simple', () => {
+    const result = getResult('FROM template/tests/query/service-template.yaml SELECT node_templates.webapp{name, type}')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/result-structure-simple/expected-output.yaml')))
+})
+
+it('result-structure-complex', () => {
+    const result = getResult('FROM template/tests/query/service-template.yaml SELECT node_templates.*{"Node Name": name, "Node Type": type}')
+    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'query/result-structure-complex/expected-output.yaml')))
+})
+
 function getResult(query: string): Object {
-    const resolver = new Resolver()
-    const args: QueryTemplateArguments = {output: '', query: query, source: 'file'}
-    return resolver.resolve(args)[0].result
+    return executeQuery({output: '', query: query, source: 'file'})
 }
