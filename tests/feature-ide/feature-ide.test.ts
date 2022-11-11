5383342b92f6820b7a899637d3c33be4dc1efe3f
Miles Stötzner <miles@stoetzner.de>
2022-11-11 22:31:17 +0100

    fix

--- tests/feature-ide/feature-ide.test.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import {expect} from 'chai'
-import * as featureIDE from '../../src/utils/feature-ide'
-import * as files from '../../src/utils/files'
-
-it('feature-ide', async () => {
-    const result = await featureIDE.loadConfiguration(path.join(__dirname, 'inputs.xml'))
-    expect(result).to.deep.equal(files.loadYAML(path.join(__dirname, 'inputs.yaml')))
-})
