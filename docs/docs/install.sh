040819c9b175b0a2ad0c3fb13e56da03e54108d7
Miles Stötzner <miles@stoetzner.de>
2023-02-08 01:30:11 +0100

    check if alpine

--- docs/docs/install.sh
+++ docs/docs/install.sh
@@ -31,14 +31,20 @@ if [ "$ARCH" = "aarch64" ]; then
   ARCH=arm64
 fi
 
-if [ ! "$ARCH" = "x64" ] || [ ! "$ARCH" = "arm64" ]; then
-  echo "platform not supported"
+if [ ! "$ARCH" = "x64" ] && [ ! "$ARCH" = "arm64" ]; then
+  echo "architecture not supported"
   exit 1
 fi
 
-wget https://github.com/opentosca/opentosca-vintner/releases/download/latest/vintner-linux-${ARCH}.xz
-tar -xf vintner-linux-${ARCH}.xz
-rm vintner-linux-${ARCH}.xz
-mv vintner-linux-${ARCH} /usr/bin/vintner
+PLAT=linux
+
+if [ -f /etc/alpine-release ]; then
+  PLAT=alpine
+fi
+
+wget https://github.com/opentosca/opentosca-vintner/releases/download/latest/vintner-${PLAT}-${ARCH}.xz
+tar -xf vintner-${PLAT}-${ARCH}.xz
+rm vintner-${PLAT}-${ARCH}.xz
+mv vintner-${PLAT}-${ARCH} /usr/bin/vintner
 chmod +x /usr/bin/vintner
 vintner setup init
