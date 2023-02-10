85ea3f1a96bde13f225058f849e25b83721ef750
Miles Stötzner <miles@stoetzner.de>
2023-02-11 00:23:42 +0100

    minor

--- src/utils/wsl.ts
+++ src/utils/wsl.ts
@@ -4,7 +4,7 @@ import {exec} from 'child_process'
 
 /**
  * Transforms windows path to WSL path.
- * For example, C:\a\b returns /mnt/c/a/b.
+ * For example, "C:\a\b" returns "/mnt/c/a/b".
  */
 function win2wsl(file: string) {
     const resolved = path.resolve(file)
@@ -15,7 +15,7 @@ function win2wsl(file: string) {
 
 /**
  * Transform WSL path to windows path.
- * For example, /mnt/c/a/b returns C:\a\b.
+ * For example, "/mnt/c/a/b" returns "\\wsl.localhost\Ubuntu-22.04\a\b".
  */
 async function wsl2win(file: string) {
     if (!wsl) throw new Error(`Can be only executed inside WSL`)
