ed3c611f142530d1e3932dcece010721ac4dfaec
Miles Stötzner <miles@stoetzner.de>
2023-02-12 23:40:25 +0100

    remove adaptation example

--- examples/xopera-self-adaptation/deploy.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/bash
-set -e
-
-yarn cli instances deploy --instance motivation --inputs examples/xopera-self-adaptation/deployment-inputs.ignored.yaml
--- examples/xopera-self-adaptation/update.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-set -e
-
-rm -rf ~/.opentosca_vintner/instances/motivation/template/lib
-cp -R lib ~/.opentosca_vintner/instances/motivation/template/lib
-cp variable-service-template.yaml ~/.opentosca_vintner/instances/motivation/template/variable-service-template.yaml
-
-yarn cli instances resolve --instance motivation --presets raspberry
