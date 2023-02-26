425468e5612f913325260dc8a9f10e1fe7f3b652
Miles Stötzner <miles@stoetzner.de>
2023-02-26 22:01:12 +0100

    init

---
+++ examples/xopera-self-adaptation/docker-pi.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+write_input() {
+  echo "$1" | socat EXEC:"docker attach vpi",pty STDIN
+}
+
+write_input 'pi'
+sleep 2
+
+write_input 'raspberry'
+sleep 3
+
+write_input 'sudo systemctl start ssh'
+sleep 2
+
+write_input 'exit'
+sleep 2
