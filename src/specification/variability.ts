ce205d551454e8a19f03b87a9eefd641e31bc22a
Miles Stötzner <miles@stoetzner.de>
2023-03-23 12:42:22 +0100

    minor

--- src/specification/variability.ts
+++ src/specification/variability.ts
@@ -15,8 +15,6 @@ export type VariabilityResolvingOptions = {
     ConsistencyOptions &
     SolverOptions
 
-// TODO: doc this
-// TODO: implement this
 export type SolverOptions = {
     optimization?: true | false | 'min' | 'max'
 }
