5a94d30bd72d897c6ed9ddd56b3bcc4a084917af
Miles Stötzner <miles@stoetzner.de>
2023-06-30 12:01:02 +0200

    patch extract-zip

--- docs/cmds/puml/generate.sh
+++ docs/cmds/puml/generate.sh
@@ -6,16 +6,16 @@ cd "$(dirname "$0")"
 cd ../../../
 
 # Default way to execute vintner is using ts-node which takes long
-VINTNER_CLI="yarn cli"
+VINTNER="yarn cli"
 
 # Optimize executing vintner by using node
 # Note, this might not be the latest build, e.g., when executed locally
-if [ -f "build/cli/index.js" ]; then
-    VINTNER_CLI="yarn vintner"
+if [ -f "build-docs/src/cli/index.js" ]; then
+    VINTNER="node build-docs/src/cli/index.js"
 fi
 
-$VINTNER_CLI template puml topology --path docs/docs/sofdcar/guides/location/service-template.yaml
-$VINTNER_CLI template puml topology --path docs/docs/sofdcar/guides/zone/service-template.yaml
+$VINTNER template puml topology --path docs/docs/sofdcar/guides/location/service-template.yaml
+$VINTNER template puml topology --path docs/docs/sofdcar/guides/zone/service-template.yaml
 
-$VINTNER_CLI template puml types --path docs/docs/sofdcar/tosca-sofdcar-profile-core.yaml
-$VINTNER_CLI template puml types --path docs/docs/sofdcar/tosca-sofdcar-profile-extended.yaml
+$VINTNER template puml types --path docs/docs/sofdcar/tosca-sofdcar-profile-core.yaml
+$VINTNER template puml types --path docs/docs/sofdcar/tosca-sofdcar-profile-extended.yaml
