b17a312c575f6d4cb449e3843f9565071bf48bc6
Miles Stötzner <miles@stoetzner.de>
2023-07-16 20:51:34 +0200

    xopera-motivation prod

---
+++ examples/xopera-motivation/scripts/demo-prod.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+set -e
+
+yarn cli setup clean
+yarn cli setup init
+yarn cli orchestrators init xopera-wsl
+yarn cli orchestrators enable --orchestrator xopera-wsl
+
+yarn cli templates import --template motivation --path examples/xopera-motivation
+yarn cli instances create --instance motivation --template motivation
+yarn cli instances resolve --instance motivation --presets prod
+yarn cli instances deploy --instance motivation --inputs examples/xopera-motivation/deployment-inputs.ignored.yaml
