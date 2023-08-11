f56bd00ceab388f1d274bedcffc5b0f9ef1594eb
Miles Stötzner <miles@stoetzner.de>
2023-08-11 22:38:09 +0200

    minor

--- src/controller/instances/continue.ts
+++ src/controller/instances/continue.ts
@@ -1,16 +1,14 @@
 import Plugins from '#plugins'
 import {Instance} from '#repository/instances'
-import * as utils from '#utils'
 import lock from '#utils/lock'
 
-export type InstancesContinueOptions = {instance: string; inputs?: string; verbose?: boolean}
+export type InstancesContinueOptions = {instance: string; verbose?: boolean}
 
 export default async function (options: InstancesContinueOptions) {
     const instance = new Instance(options.instance)
 
     await lock.try(instance.getLockKey(), async () => {
         if (!instance.exists()) throw new Error(`Instance "${instance.getName()}" does not exist`)
-        instance.setServiceInputs(utils.now(), options.inputs)
         await Plugins.getOrchestrator().continue(instance, {verbose: options.verbose})
     })
 }
