db11104f34b46e8283f3d6e6b755854206f2d546
Miles Stötzner <miles@stoetzner.de>
2023-09-13 23:32:44 +0200

    unfurl artifacts demo

---
+++ examples/unfurl-artifacts/demo/after.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+set -e
+
+# Set working directory
+cd "$(dirname "$0")"
+cd ../
+
+# Undeploy
+vintner instances undeploy --instance artifacts
+
+# Cleanup
+vintner setup clean --force
+
+# Remove vintner
+sudo npm uninstall -g opentosca-vintner
