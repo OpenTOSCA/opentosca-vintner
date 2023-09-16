942315aea2f1cee3684b38fa239efc721f4f8215
Miles Stötzner <miles@stoetzner.de>
2023-09-16 19:48:28 +0200

    fix

--- examples/xopera-pruning/scripts/deploy.sh
+++ examples/xopera-pruning/scripts/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 yarn cli templates import --template pruning --path examples/xopera-pruning
 yarn cli instances init --instance pruning --template pruning
-yarn cli instances resolve --instance pruning --presets static
+yarn cli instances resolve --instance pruning --presets elastic
 yarn cli instances deploy --instance pruning --inputs examples/xopera-pruning/deployment-inputs.ignored.yaml
--- examples/xopera-pruning/scripts/reimport.sh
+++ examples/xopera-pruning/scripts/reimport.sh
@@ -6,5 +6,5 @@ yarn cli templates delete --template pruning
 
 yarn cli templates import --template pruning --path examples/xopera-pruning
 yarn cli instances init --instance pruning --template pruning
-yarn cli instances resolve --instance pruning --presets static
+yarn cli instances resolve --instance pruning --presets elastic
 yarn cli instances deploy --instance pruning --inputs examples/xopera-pruning/deployment-inputs.ignored.yaml
