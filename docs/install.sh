9f7bdce783e5aaba303b17ecb592d243106b1f27
Miles Stötzner <miles@stoetzner.de>
2023-10-04 15:35:08 +0200

    minor

--- docs/install.sh
+++ docs/install.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
 sudo apt-get update -y
 
 # Install requirements for PlantUML
--- examples/.debug/run-playbook.sh
+++ examples/.debug/run-playbook.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
 ansible-playbook -vv -i inventory.yaml -e @inputs.json delete.yaml
