626b9f15de7fa03b1d6f98b211904fee7a6cbeeb
Miles Stötzner <miles@stoetzner.de>
2023-10-08 12:36:20 +0200

    some validates

--- src/assets/templates/template-init/scripts/reimport.sh
+++ src/assets/templates/template-init/scripts/reimport.sh
@@ -10,4 +10,5 @@ $VINTNER templates delete --template ${TEMPLATE_NAME}
 $VINTNER templates import --template ${TEMPLATE_NAME}  --path ${TEMPLATE_DIR}
 $VINTNER instances init --instance ${TEMPLATE_NAME}  --template ${TEMPLATE_NAME}
 $VINTNER instances resolve --instance ${TEMPLATE_NAME}  --inputs ${TEMPLATE_DIR}/tests/first/inputs.yaml
+$VINTNER instances validate --instance ${TEMPLATE_NAME}
 $VINTNER instances deploy --instance ${TEMPLATE_NAME}  --inputs ${TEMPLATE_DIR}/deployment-inputs.ignored.yaml
--- tests/integration/adaptation.test.ts
+++ tests/integration/adaptation.test.ts
@@ -50,6 +50,7 @@ if (!integrationTestsEnabled) {
                 instance: instanceName,
                 inputs: path.join(templateDirectory, 'variability-inputs.example.yaml'),
             })
+            await Controller.instances.validate({instance: instanceName})
 
             // Assert that variability-resolved service template and variability inputs are as expected
             expect(instance.loadServiceTemplate()).to.deep.equal(firstTemplate)
@@ -65,6 +66,7 @@ if (!integrationTestsEnabled) {
                 instance: instanceName,
                 inputs: {mode: 'second'},
             })
+            await Controller.instances.validate({instance: instanceName})
 
             // Wait until adaptation finished
             await utils.sleep(5 * 1000)
@@ -79,6 +81,7 @@ if (!integrationTestsEnabled) {
                     instance: instanceName,
                     inputs: {mode: value},
                 })
+                await Controller.instances.validate({instance: instanceName})
             }
 
             // Wait until everything is adapted
