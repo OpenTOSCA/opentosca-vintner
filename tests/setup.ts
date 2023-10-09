733ad6eddf35964655d2243600ec2c500f9f5edd
Miles Stötzner <miles@stoetzner.de>
2023-10-09 21:07:00 +0200

    load source-map-support at nyc

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,9 @@
 import chai from 'chai'
 import chaiDatetime from 'chai-datetime'
 chai.use(chaiDatetime)
+
+// See https://github.com/istanbuljs/nyc/issues/619#issuecomment-754609188
+import sms from 'source-map-support'
+sms.install({
+    hookRequire: true,
+})
