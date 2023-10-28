f8934eb4b86432f9c3cc83c96df7d314094bc1a5
Miles Stötzner <miles@stoetzner.de>
2023-10-28 12:46:27 +0200

    minor

--- examples/unfurl-artifacts/scripts/deploy.sh
+++ examples/unfurl-artifacts/scripts/deploy.sh
@@ -6,5 +6,5 @@ source configuration.sh
 
 $VINTNER templates import --template ${TEMPLATE_NAME} --path ${TEMPLATE_DIR}
 $VINTNER instances init --instance ${TEMPLATE_NAME} --template ${TEMPLATE_NAME}
-$VINTNER instances resolve --instance ${TEMPLATE_NAME} --inputs ${TEMPLATE_DIR}/tests/enterprise/inputs.yaml
+$VINTNER instances resolve --instance ${TEMPLATE_NAME} --inputs ${TEMPLATE_DIR}/tests/${DEPLOYMENT_VARIANT}/inputs.yaml
 $VINTNER instances deploy --instance ${TEMPLATE_NAME} --inputs ${TEMPLATE_DIR}/deployment-inputs.ignored.yaml
--- examples/unfurl-artifacts/scripts/setup.sh
+++ examples/unfurl-artifacts/scripts/setup.sh
@@ -4,6 +4,7 @@ set -e
 # Load configuration
 source configuration.sh
 
-$VINTNER cli setup init
-$VINTNER cli orchestrators init unfurl --no-env
-$VINTNER cli orchestrators enable --orchestrator unfurl
+$VINTNER setup init
+$VINTNER orchestrators init unfurl --no-venv
+$VINTNER orchestrators enable --orchestrator unfurl
+$VINTNER orchestrators attest --orchestrator unfurl
