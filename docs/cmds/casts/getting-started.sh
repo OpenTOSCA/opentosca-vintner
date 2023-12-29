939c8bb86f889e0bc8ddaf2d1395fe3ff2ffd9d1
Miles Stötzner <miles@stoetzner.de>
2023-12-29 16:46:23 +0100

    minor

--- docs/cmds/casts/getting-started.sh
+++ docs/cmds/casts/getting-started.sh
@@ -18,7 +18,8 @@ echo '# Installation'
 p 'curl -fsSL https://vintner.opentosca.org/install.sh | sudo bash -'
 sleep 3
 
-pe 'vintner setup init'
+# Is part of the installation
+vintner setup init
 
 echo ''
 echo '# Orchestrator'
