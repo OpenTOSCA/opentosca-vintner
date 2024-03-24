a11d82f28851874c010de9659a5844de802df80a
Miles Stötzner <miles@stoetzner.de>
2024-03-24 19:33:26 +0100

    minor

---
+++ tasks/git/chown/task.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+set -e
+
+# Set working directory
+cd "$(dirname "$0")"
+cd ../../../
+
+# Set PATH
+PATH="$(pwd)/node_modules/.bin:$PATH"
+
+# TODO: chown shell scripts (update git-index)
---
+++ tasks/git/dos2unix/task.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+set -e
+
+# Set working directory
+cd "$(dirname "$0")"
+cd ../../../
+
+# Set PATH
+PATH="$(pwd)/node_modules/.bin:$PATH"
+
+# TODO: dos2unix of shell scripts
