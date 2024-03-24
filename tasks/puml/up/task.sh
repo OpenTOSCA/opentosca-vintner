8b8edea9638210a66ff0ce5fc281a23136692945
Miles Stötzner <miles@stoetzner.de>
2024-03-24 13:02:55 +0100

    puml

---
+++ tasks/puml/up/task.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+set -e
+
+# Set working directory
+cd "$(dirname "$0")"
+cd ../../../
+
+# Set PATH
+PATH="$(pwd)/node_modules/.bin:$PATH"
+
+# Start PUML server
+cd docs && docker-compose down
