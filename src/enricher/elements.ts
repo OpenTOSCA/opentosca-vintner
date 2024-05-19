7fb7c9c396f04783a64d0923e87ee24874c99e4f
Miles Stötzner <miles@stoetzner.de>
2024-05-19 15:49:04 +0200

    fix

--- src/enricher/elements.ts
+++ src/enricher/elements.ts
@@ -20,16 +20,20 @@ export class ElementEnricher {
             // Do not override manual modelled technologies
             if (!utils.isEmpty(node.technologies)) continue
 
+            let hasCandidate = false
             for (const plugin of this.graph.plugins.technology) {
                 for (const map of plugin.assign(node)) {
+                    hasCandidate = true
+                    // Note, this does not update node.technologies
                     this.graph.addTechnology(node, map)
                 }
             }
 
             // Throw if technology is required but no candidate has been found
             if (this.graph.options.checks.requiredTechnology) {
-                if (node.technologyRequired && utils.isEmpty(node.technologies))
+                if (node.technologyRequired && !hasCandidate) {
                     throw new Error(`${node.Display} has no technology candidates`)
+                }
             }
         }
     }
