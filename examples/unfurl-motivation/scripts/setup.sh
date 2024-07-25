531de9fa6f7888318273c5d11bbeb1404a9859e8
Miles Stötzner <miles@stoetzner.de>
2024-07-26 00:31:33 +0200

    set ready state at unfurl create

--- examples/unfurl-motivation/scripts/setup.sh
+++ examples/unfurl-motivation/scripts/setup.sh
@@ -5,6 +5,6 @@ set -e
 source configuration.sh
 
 $VINTNER setup init
-$VINTNER orchestrators init unfurl-wsl
-$VINTNER orchestrators enable --orchestrator unfurl-wsl
-$VINTNER orchestrators attest --orchestrator unfurl-wsl
+$VINTNER orchestrators init unfurl --dir ~/unfurl-v1.1.0
+$VINTNER orchestrators enable --orchestrator unfurl
+$VINTNER orchestrators attest --orchestrator unfurl
