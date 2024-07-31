58fe1020b791be4c9eb332b86a217e7b18ac19db
Miles Stötzner <miles@stoetzner.de>
2024-07-31 10:46:01 +0200

    drop new example for now

--- examples/unfurl-technology---gcp/scripts/enrich.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Load configuration
-source configuration.sh
-
-# Enrich
-$VINTNER template enrich --template ${TEMPLATE_DIR}/variable-service-template.yaml --output ${TEMPLATE_DIR}/enriched.yaml
