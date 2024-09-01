c0ce3e2ce10d9c72af4904da9aa2631c6e85f60e
Miles Stötzner <miles@stoetzner.de>
2024-09-01 23:38:32 +0200

    task postinall

--- tasks/install/task.sh
+++ tasks/install/task.sh
@@ -2,19 +2,3 @@
 echo "Installing dependencies ..."
 yarn --immutable
 echo "Done"
-
-# Patch packages
-echo
-echo "Patching dependencies ..."
-patch-package
-echo "Done"
-
-# Patch binary
-echo
-echo "Patching binaries..."
-echo "node-plantuml"
-PLANTUML_JAR=node_modules/node-plantuml/vendor/plantuml.1.2023.7.jar
-if [[ ! -f "${PLANTUML_JAR}" ]]; then
-  wget -O ${PLANTUML_JAR} https://sourceforge.net/projects/plantuml/files/1.2023.7/plantuml.1.2023.7.jar
-fi
-echo "Done"
