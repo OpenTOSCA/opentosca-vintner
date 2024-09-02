691e808637ca2c678091af94e570f8abe8d3b49d
Miles Stötzner <miles@stoetzner.de>
2024-09-02 16:25:57 +0200

    kubectl port-forward

--- src/technologies/plugins/rules/generators/mysql-database/ansible/mysql-dbms/kubernetes-cluster/index.ts
+++ src/technologies/plugins/rules/generators/mysql-database/ansible/mysql-dbms/kubernetes-cluster/index.ts
@@ -1,3 +1,4 @@
+import * as files from '#files'
 import {ImplementationGenerator} from '#technologies/plugins/rules/types'
 import {
     AnsibleOrchestratorOperation,
@@ -10,7 +11,7 @@ import {
 
 // TODO: use  https://kubernetes.io/docs/tasks/access-application-cluster/port-forward-access-application-cluster/ ?
 
-// https://stackoverflow.com/questions/37288500/how-to-undo-a-kubectl-port-forward
+// TODO: use https://stackoverflow.com/questions/37288500/how-to-undo-a-kubectl-port-forward
 
 const generator: ImplementationGenerator = {
     component: 'mysql.database',
@@ -40,27 +41,72 @@ const generator: ImplementationGenerator = {
                                 playbook: {
                                     q: [
                                         {
+                                            name: 'create service',
+                                            'ansible.builtin.copy': {
+                                                dest: '/etc/systemd/system/kubectl-port-forward.service',
+                                                content: files.toINI({
+                                                    Unit: {
+                                                        After: 'network.target',
+                                                    },
+                                                    Service: {
+                                                        Type: 'simple',
+                                                        ExecStart: `kubectl port-forward service/{{ HOST.dbms_name }} 23306:3306"`,
+                                                    },
+                                                    Install: {
+                                                        WantedBy: 'multi-user.target',
+                                                    },
+                                                }),
+                                            },
+                                        },
+                                        {
+                                            name: 'enable service',
+                                            'ansible.builtin.systemd': {
+                                                name: 'kubectl-port-forward',
+                                                state: 'started',
+                                                daemon_reload: 'yes',
+                                            },
+                                        },
+                                        {
                                             name: 'create database',
-                                            'ansible.builtin.shell':
-                                                'kubectl exec deploy/{{ HOST.dbms_name }} -- mysql --password={{ HOST.dbms_password }} -e "CREATE DATABASE IF NOT EXISTS {{ SELF.database_name }}"',
-                                            args: {
-                                                executable: '/usr/bin/bash',
+                                            'community.mysql.mysql_db': {
+                                                name: '{{ SELF.database_name }}',
+                                                login_host: '127.0.0.1',
+                                                login_password: '{{ HOST.dbms_password }}',
+                                                login_port: '23306',
+                                                login_user: 'root',
                                             },
                                         },
                                         {
-                                            name: 'create user',
-                                            'ansible.builtin.shell':
-                                                "kubectl exec deploy/{{ HOST.dbms_name }}  -- mysql --password={{ HOST.dbms_password }} -e \"CREATE USER IF NOT EXISTS '{{ SELF.database_user }}'@'%' IDENTIFIED BY '{{ SELF.database_password }}'\"",
-                                            args: {
-                                                executable: '/usr/bin/bash',
+                                            name: 'create user (with privileges)',
+                                            'community.mysql.mysql_user': {
+                                                name: '{{ SELF.database_user }}',
+                                                password: '{{ SELF.database_password }}',
+                                                host: '%',
+                                                priv: '*.*:ALL',
+                                                login_host: '127.0.0.1',
+                                                login_password: '{{ HOST.dbms_password }}',
+                                                login_port: '23306',
+                                                login_user: 'root',
                                             },
                                         },
                                         {
-                                            name: 'grant privileges',
-                                            'ansible.builtin.shell':
-                                                "kubectl exec deploy/{{ HOST.dbms_name }}  -- mysql --password={{ HOST.dbms_password }} -e \"GRANT ALL PRIVILEGES ON *.* TO '{{ SELF.database_user }}'@'%'\"",
-                                            args: {
-                                                executable: '/usr/bin/bash',
+                                            name: 'stop service',
+                                            'ansible.builtin.systemd': {
+                                                name: 'kubectl-port-forward',
+                                                state: 'stopped',
+                                            },
+                                        },
+                                        {
+                                            name: 'delete systemd service',
+                                            'ansible.builtin.file': {
+                                                path: '/etc/systemd/system/kubectl-port-forward.service',
+                                                state: 'absent',
+                                            },
+                                        },
+                                        {
+                                            name: 'reload daemon',
+                                            'ansible.builtin.systemd': {
+                                                daemon_reload: true,
                                             },
                                         },
                                     ],
