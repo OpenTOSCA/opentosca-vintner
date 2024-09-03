98f9e1b7e82a1a8a6d5db18d0e74bf7b699b31e8
Miles Stötzner <miles@stoetzner.de>
2024-09-03 19:02:49 +0200

    minor

--- src/technologies/plugins/rules/generators/service-application/index.ts
+++ src/technologies/plugins/rules/generators/service-application/index.ts
@@ -1,5 +1,5 @@
 import dockerImage from './docker-image'
-import tarArchive from './tar-archive/asterisk/virtual-machine'
+import tarArchive from './tar-archive'
 import zipArchive from './zip-archive'
 
 export default [dockerImage, tarArchive, zipArchive]
