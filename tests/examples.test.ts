6ae93e65a09b71e01fdeebd639e7fe8467e9a4a8
Miles Stötzner <miles@stoetzner.de>
2024-09-22 02:05:30 +0200

    improve

--- tests/examples.test.ts
+++ tests/examples.test.ts
@@ -13,7 +13,7 @@ describe('examples', async () => {
 
         for (const example of examples) {
             // TODO: remove this
-            if (!example.includes('industry---plus-maintenance-automated')) continue
+            if (!example.includes('industry---plus-maintenance-manual')) continue
 
             const exampleDir = path.join(examplesDir, example)
             const testsPath = path.join(exampleDir, 'tests')
