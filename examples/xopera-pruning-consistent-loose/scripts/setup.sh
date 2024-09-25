522809222f3b095cff8a307ef6ca9dde3964c9d2
Miles Stötzner <miles@stoetzner.de>
2024-09-25 14:30:53 +0200

    upgrade openstacksdk at xopera

--- examples/xopera-pruning-consistent-loose/scripts/setup.sh
+++ examples/xopera-pruning-consistent-loose/scripts/setup.sh
@@ -5,6 +5,6 @@ set -e
 source configuration.sh
 
 $VINTNER setup init
-$VINTNER orchestrators init xopera --no-venv
+$VINTNER orchestrators init xopera
 $VINTNER orchestrators enable --orchestrator xopera
 $VINTNER orchestrators attest --orchestrator xopera
