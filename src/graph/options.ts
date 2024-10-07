067eb65f8bfed876ba06fbbc010b10dccfe725c7
Miles Stötzner <miles@stoetzner.de>
2024-10-07 23:32:13 +0200

    minor

--- src/graph/options.ts
+++ src/graph/options.ts
@@ -925,7 +925,7 @@ class ConstraintsOptions extends BaseOptions {
         }
 
         if (this.v3) {
-            this.requiredArtifact = this.raw.required_artifact_constraint ?? this.constraints ?? true
+            this.requiredArtifact = this.raw.required_artifact_constraint ?? this.raw.constraints ?? true
         } else {
             this.requiredArtifact = this.raw.required_artifact_constraint ?? this.constraints
         }
