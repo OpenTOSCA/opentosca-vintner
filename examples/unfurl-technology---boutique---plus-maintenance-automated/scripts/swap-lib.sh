89cb42d38def1f5abe434f1c88ec1d92b7b39eec
Miles Stötzner <miles@stoetzner.de>
2024-11-19 18:13:28 +0100

    fixes

--- examples/unfurl-technology---boutique---plus-maintenance-automated/scripts/swap-lib.sh
+++ examples/unfurl-technology---boutique---plus-maintenance-automated/scripts/swap-lib.sh
@@ -4,13 +4,16 @@ set -e
 # Set working directory
 cd "$(dirname "$0")"
 
+# Load configuration
+source configuration.sh
+
 FS="/c/Users"
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
   FS="/home"
 fi
 
 # Delete lib in instance
-rm -rf ${FS}/stoetzms/.opentosca_vintner/instances/technology-gcp/data/ensemble/lib
+rm -rf ${FS}/stoetzms/.opentosca_vintner/instances/${TEMPLATE_NAME}/data/ensemble/lib
 
 # Copy local lib into instance
-cp -R ../lib ${FS}/stoetzms/.opentosca_vintner/instances/technology-gcp/data/ensemble
+cp -R ../lib ${FS}/stoetzms/.opentosca_vintner/instances/${TEMPLATE_NAME}/data/ensemble
