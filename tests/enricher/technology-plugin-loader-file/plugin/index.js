6e6714c902d19d137f787c52eb37607cec6ee501
Miles Stötzner <miles@stoetzner.de>
2024-11-25 23:48:41 +0100

    fix

--- tests/enricher/technology-plugin-loader-file/plugin/index.js
+++ tests/enricher/technology-plugin-loader-file/plugin/index.js
@@ -5,6 +5,7 @@ const technology = require("technology");
 module.exports = {
     build: (graph) => {
         return {
+            backwards: () => true,
             assign: (node) => {
                 return [{ [technology.name()]: { assign: "this-is-assigned" } }];
             }
