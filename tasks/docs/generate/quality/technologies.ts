69c36d1e3ed55bbca0311d85c5d15fe9452703d9
Miles Stötzner <miles@stoetzner.de>
2025-03-31 13:45:57 +0200

    docs: hide qualities for study

--- tasks/docs/generate/quality/technologies.ts
+++ tasks/docs/generate/quality/technologies.ts
@@ -9,7 +9,7 @@ const ansible: TechnologyDescription = {
     id: 'ansible',
     name: 'Ansible',
     description:
-        'Ansible is an automation tool primarily used for configuration management, application deployment, and task automation. It allows system administrators and DevOps teams to manage software on servers using SSH, without the need for agents on target nodes. Ansible uses simple, human-readable YAML files called playbooks to define tasks and configurations, enabling consistent management of IT environments across multiple servers.',
+        'Ansible is an automation tool that can be used for configuration management, application deployment, and task automation. It allows system administrators and DevOps teams to manage software on servers using SSH, without the need for agents on target nodes. Further, Ansible is capable of managing infrastructure components, such as virtual machines, networks, and storage, and supports a wide range of cloud providers, such as AWS, Azure, Google Cloud and many others.',
     link: 'https://ansible.com',
 }
 
@@ -17,7 +17,7 @@ const terraform: TechnologyDescription = {
     id: 'terraform',
     name: 'Terraform',
     description:
-        'Terraform is an infrastructure as code (IaC) tool that allows users to define, provision, and manage cloud infrastructure in a consistent, automated, and reproducible way. Using a declarative configuration language called HashiCorp Configuration Language (HCL), Terraform enables users to define infrastructure components such as virtual machines, networks, and storage in human-readable configuration files. It supports a wide range of cloud providers, including AWS, Azure, Google Cloud, and many others, allowing for seamless multi-cloud management. By applying these configurations, Terraform creates and manages the defined resources through APIs.',
+        'Terraform is an infrastructure as code (IaC) tool that allows users to define, provision, and manage cloud infrastructure. Using a declarative configuration language called HashiCorp Configuration Language (HCL), Terraform enables users to define infrastructure components such as virtual machines, networks, and storage in human-readable configuration files. It supports a wide range of cloud providers, including AWS, Azure, Google Cloud, and many others.',
     link: 'https://terraform.io',
 }
 
@@ -25,7 +25,7 @@ const kubernetes: TechnologyDescription = {
     id: 'kubernetes',
     name: 'Kubernetes',
     description:
-        'Kubernetes is a system for automating the deployment, scaling, and management of containerized applications, offering production-grade container orchestration. It provides a platform for running and managing applications in clusters of servers, ensuring high availability, scalability, and efficient resource utilization. Kubernetes allows users to define the desired state of their applications using declarative manifests, which specify the configuration, deployment, and management of containerized workloads. By continuously monitoring and adjusting the cluster to match these desired states, Kubernetes simplifies the application deployment process, supports automated rollouts and rollbacks, and ensures the self-healing of applications.',
+        'Kubernetes is a platform for automating the deployment, scaling, and management of containerized applications, such as Docker Containers. It provides capabilities for running and managing applications in clusters of servers, ensuring high availability, scalability, and efficient resource utilization. Kubernetes allows to define the desired state of their applications using declarative manifests, which specify the configuration, deployment, and management of containerized workloads.',
     link: 'https://kubernetes.io',
 }
 
@@ -33,7 +33,7 @@ const compose: TechnologyDescription = {
     id: 'compose',
     name: 'Docker Compose',
     description:
-        'Docker Compose is a tool that enables the definition and running of multi-container applications on a Docker Engine, using a single YAML configuration file. It allows the management of services, networks, and volumes, in a declarative manner. Compose is particularly suited for single-host deployments, providing a consistent environment across various stages of the application lifecycle. However, it is limited to working within the Docker ecosystem.',
+        'Docker Compose is a tool that enables the definition and running of multi-container applications on a Docker Engine, using a single YAML configuration file. It allows the management of services, networks, and volumes, in a declarative manner.',
     link: 'https://docs.docker.com/compose',
 }
 
